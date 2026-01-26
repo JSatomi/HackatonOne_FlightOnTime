07cd774b3bebbe02e2f852d06dfd87759c466f51
stonedjjh <danjim82@gmail.com>
2026-01-25 22:55:22 -0400

    feat: añadir datos de aerolíneas a la base de datos

---
+++ src/main/resources/db/migration/V7__insert_airlines_data.sql
@@ -0,0 +1,11 @@
+INSERT IGNORE INTO airlines (code, name) VALUES 
+('UA', 'United Air Lines Inc.'),
+('WN', 'Southwest Airlines Co.'),
+('AA', 'American Airlines Inc.'),
+('DL', 'Delta Air Lines Inc.'),
+('B6', 'JetBlue Airways'),
+('F9', 'Frontier Airlines Inc.'),
+('G4', 'Allegiant Air'),
+('HA', 'Hawaiian Airlines Inc.'),
+('NK', 'Spirit Airlines'),
+('AS', 'Alaska Airlines Inc.');
